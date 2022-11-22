fcacf98d28e846942f6ad9ca671dfc1d2d707ac8
yeasirh <yeasirh@gmail.com>
2022-11-22 15:51:34 +1100

    retrieving cursor position on click

--- challenge01/src/App.js
+++ challenge01/src/App.js
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+	const [mousePos, setMousePos] = useState({});
+
+	useEffect(() => {
+		const handleMouseMove = (event) => {
+			setMousePos({ x: event.clientX, y: event.clientY });
+		};
+
+		window.addEventListener("mousemove", handleMouseMove);
+
+		return () => {
+			window.removeEventListener("mousemove", handleMouseMove);
+		};
+	}, []);
+
+	const savePosition = () => {
+		console.log(mousePos.x, mousePos.y);
+	};
+
 	return (
-		<div className="App">
-			<h1>Hello world</h1>
+		<div className="App" onClick={savePosition}>
+			The mouse is at position{" "}
+			<b>
+				({mousePos.x},{mousePos.y})
+			</b>
 		</div>
 	);
 }
