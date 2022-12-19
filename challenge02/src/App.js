4d470aa504e31932fa75315d6ad93fc3dc77939a
yeasirh <yeasirh@gmail.com>
2022-12-19 16:04:47 +1100

    color game working

--- challenge02/src/App.js
+++ challenge02/src/App.js
@@ -1,7 +1,50 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-	return <h1>Hello world</h1>;
+	const [color, setColor] = useState();
+	const [answers, setAnswers] = useState([]);
+	const [correctAnswer, setcorrectAnswer] = useState(undefined);
+
+	const getRandomColor = () => {
+		return "#" + Math.floor(Math.random() * 16777215).toString(16);
+	};
+
+	const checkAnswer = (answer) => {
+		if (answer === color) {
+			setcorrectAnswer(true);
+			generateColor();
+		} else {
+			setcorrectAnswer(false);
+		}
+	};
+
+	const generateColor = () => {
+		const actualColor = getRandomColor();
+		setColor(actualColor);
+		setAnswers(
+			[actualColor, getRandomColor(), getRandomColor()].sort(
+				() => Math.random() - 0.5
+			)
+		);
+	};
+
+	useEffect(() => {
+		generateColor();
+		setcorrectAnswer(undefined);
+	}, []);
+	return (
+		<div>
+			<div className="guess-me" style={{ background: color }}></div>
+			{answers.map((answer) => (
+				<button onClick={() => checkAnswer(answer)} key={answer}>
+					{answer}
+				</button>
+			))}
+			{correctAnswer === true && <div>Correct Answer</div>}
+			{correctAnswer === false && <div>Wrong Answer</div>}
+		</div>
+	);
 }
 
 export default App;
